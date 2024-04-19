fda429be6c5dc8ef6617c0255651d88808aa58a4
tthjani <tothje98@gmail.com>
2024-04-19 16:50:10 +0200

    feat: added Announcement model for the database

---
+++ APhoto-backend/Models/Announcement.cs
@@ -0,0 +1,8 @@
+﻿namespace APhoto_backend.Models;
+
+public class Announcement
+{
+    public uint Id { get; set; }
+    public string Message { get; set; }
+    public string ImageUrl { get; set; }
+}
