3c0ab34a80893602b3ee7779828d30b3a698fb89
tthjani <tothje98@gmail.com>
2024-04-19 16:48:48 +0200

    feat: added Order model for the database

---
+++ APhoto-backend/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace APhoto_backend.Models;
+
+public class Order
+{
+    public uint Id { get; set; }
+    public DateTime Date { get; set; }
+    public string Type { get; set; }
+    public string ClientName { get; set; }
+    public string ClientEmail { get; set; }
+    public string FbUrl { get; set; }
+    public string Message { get; set; }
+    public bool IsAccepted { get; } = false;
+    public bool IsDeclined { get; } = false;
+}
