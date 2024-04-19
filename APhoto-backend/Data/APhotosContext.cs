520241ce15e0bde07da3f82e709c2ff18cb012fb
tthjani <tothje98@gmail.com>
2024-04-19 16:51:13 +0200

    feat: added APhotosContext to the backend

---
+++ APhoto-backend/Data/APhotosContext.cs
@@ -0,0 +1,21 @@
+﻿using APhoto_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APhoto_backend.Data;
+
+public class APhotosContext : DbContext
+{
+
+    public APhotosContext(DbContextOptions<APhotosContext> options) : base(options)
+    {
+    }
+    
+
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Gallery> Galleries { get; set; }
+    public DbSet<Holiday> Holidays { get; set; }
+    public DbSet<AcceptedOrder> AcceptedOrders { get; set; }
+    public DbSet<Announcement> Announcements { get; set; }
+    public DbSet<FinishedOrder> FinishedOrders { get; set; }
+    public DbSet<DeclinedOrder> DeclinedOrders { get; set; }
+}
