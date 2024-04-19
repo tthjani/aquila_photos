8afb4f91bc07249b67ac2b518e9c93009ffbc811
tthjani <tothje98@gmail.com>
2024-04-19 16:49:29 +0200

    feat: added FinishedOrder model for the database

---
+++ APhoto-backend/Models/FinishedOrder.cs
@@ -0,0 +1,8 @@
+﻿namespace APhoto_backend.Models;
+
+public class FinishedOrder
+{
+    public uint Id { get; set; }
+    public uint AcceptedId { get; set; }
+    public DateTime FinishDate { get; set; }
+}
