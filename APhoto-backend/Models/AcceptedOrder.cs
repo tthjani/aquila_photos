757359075289c33c411c22111d0d2a27b55b437e
tthjani <tothje98@gmail.com>
2024-04-19 16:49:08 +0200

    feat: added AcceptedOrder model for the database

---
+++ APhoto-backend/Models/AcceptedOrder.cs
@@ -0,0 +1,9 @@
+﻿namespace APhoto_backend.Models;
+
+public class AcceptedOrder
+{
+    public uint Id { get; set; }
+    public uint OrderId { get; set; }
+    public DateTime AcceptanceDate { get; set; }
+    public bool IsFinished { get; } = false;
+}
