bf69b0488ed79d843f57f321cf93bd540f790951
tthjani <tothje98@gmail.com>
2024-04-19 16:49:53 +0200

    feat: added Holiday model for the database

---
+++ APhoto-backend/Models/Holiday.cs
@@ -0,0 +1,9 @@
+﻿
+namespace APhoto_backend.Models;
+
+public class Holiday
+{
+    public uint Id { get; set; }
+    public DateTime StartHoliday { get; set; }
+    public DateTime EndHoliday { get; set; }
+}
