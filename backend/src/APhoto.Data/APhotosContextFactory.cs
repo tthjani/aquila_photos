5446f640f093389162c37dcd98df81659261c81a
Gergely Illyes <gergo.illyes@gmail.com>
2024-05-04 17:03:01 +0200

    Enabling EF migration creation

---
+++ backend/src/APhoto.Data/APhotosContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APhoto.Data
+{
+    /// <summary>
+    /// DbContextFactory for dotnet-ef migrations
+    /// </summary>
+    public class APhotosContextFactory : IDesignTimeDbContextFactory<APhotosContext>
+    {
+        public APhotosContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<APhotosContext>();
+            optionsBuilder.UseSqlServer(args[0]);
+
+            return new APhotosContext(optionsBuilder.Options);
+        }
+    }
+}
