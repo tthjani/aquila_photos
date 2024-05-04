644e7df61c3a757cd258aab5469f72b7dd0995aa
tthjani <tothje98@gmail.com>
2024-05-04 12:24:16 +0200

    feat: added IAcceptedOrdersRepository interface

---
+++ APhoto-backend/Services/Repository/IAcceptedOrdersRepository.cs
@@ -0,0 +1,11 @@
+﻿using APhoto_backend.Models;
+
+namespace APhoto_backend.Services.Repository;
+
+public interface IAcceptedOrdersRepository
+{
+    Task<List<AcceptedOrder>> GetAllAcceptedOrders();
+    void AddAcceptedOrder(AcceptedOrder acceptedOrder);
+    AcceptedOrder GetOrderById(uint orderId);
+    void FinishOrder(AcceptedOrder accOrder);
+}
