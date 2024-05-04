8cd554fd31763612d36013f734261d21a80d2c5c
tthjani <tothje98@gmail.com>
2024-05-04 12:24:54 +0200

    feat: added IFinishedOrdersRepository interface

---
+++ APhoto-backend/Services/Repository/IFinishedOrdersRepository.cs
@@ -0,0 +1,9 @@
+﻿using APhoto_backend.Models;
+
+namespace APhoto_backend.Services.Repository;
+
+public interface IFinishedOrdersRepository
+{
+    Task<List<FinishedOrder>> GetAllFinishedOrders();
+    void AddFinishedOrder(FinishedOrder finishedOrder);
+}
