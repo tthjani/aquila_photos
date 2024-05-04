6fdf458cc3ab17183a4286f81f285733d1a4effa
tthjani <tothje98@gmail.com>
2024-05-04 12:23:44 +0200

    feat: added DeclinedOrdersRepository

---
+++ APhoto-backend/Services/Repository/DeclinedOrdersRepository.cs
@@ -0,0 +1,25 @@
+﻿using APhoto_backend.Data;
+using APhoto_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APhoto_backend.Services.Repository;
+
+public class DeclinedOrdersRepository : IDeclinedOrdersReporitory
+{
+    private readonly APhotosContext _context;
+
+    public DeclinedOrdersRepository(APhotosContext context)
+    {
+        _context = context;
+    }
+    
+    public Task<List<DeclinedOrder>> GetAllDeclinedOrders()
+    {
+        return _context.DeclinedOrders.ToListAsync();
+    }
+    public void AddDeclinedOrder(DeclinedOrder declinedOrder)
+    {
+        _context.DeclinedOrders.Add(declinedOrder);
+        _context.SaveChanges();
+    }
+}
