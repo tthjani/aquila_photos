83f8e6b47db04b079ead189b60402d3d6704b3c0
tthjani <tothje98@gmail.com>
2024-05-04 12:24:37 +0200

    feat: added IDeclinedOrdersReporitory interface

---
+++ APhoto-backend/Services/Repository/IDeclinedOrdersReporitory.cs
@@ -0,0 +1,9 @@
+﻿using APhoto_backend.Models;
+
+namespace APhoto_backend.Services.Repository;
+
+public interface IDeclinedOrdersReporitory
+{
+    Task<List<DeclinedOrder>> GetAllDeclinedOrders();
+    void AddDeclinedOrder(DeclinedOrder declinedOrder);
+}
