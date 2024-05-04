48d6419171242f586564fd64a7f403fd2f5477a1
tthjani <tothje98@gmail.com>
2024-05-04 12:25:34 +0200

    feat: added more methods to IOrdersRepository interface

--- APhoto-backend/Services/Repository/IOrdersRepository.cs
+++ APhoto-backend/Services/Repository/IOrdersRepository.cs
@@ -6,4 +6,9 @@ public interface IOrdersRepository
 {
     Task<List<Order>> GetAllOrders();
     void Add(Order order);
+    void DeleteOrder(Order order);
+    void AcceptOrder(Order order);
+    void DeclineOrder(Order order);
+    Order GetOrderById(uint orderId);
+    void UpdateOrder(Order order);
 }
