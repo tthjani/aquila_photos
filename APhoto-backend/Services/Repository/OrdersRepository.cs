a8fec81c62ca7e8b2cc617a46b7868c0b425f214
tthjani <tothje98@gmail.com>
2024-05-04 12:26:11 +0200

    feat: implemented the methods from the interface

--- APhoto-backend/Services/Repository/OrdersRepository.cs
+++ APhoto-backend/Services/Repository/OrdersRepository.cs
@@ -24,4 +24,27 @@ public class OrdersRepository : IOrdersRepository
         _context.Add(order);
         _context.SaveChanges();
     }
+
+    public void AcceptOrder(Order order)
+    {
+        order.IsAccepted = true;
+    }
+
+    public void DeclineOrder(Order order)
+    {
+        order.IsDeclined = true;
+    }
+    public void DeleteOrder(Order order)
+    {
+        _context.Remove(order);
+        _context.SaveChanges();
+    }
+    public Order GetOrderById(uint orderId)
+    {
+        return _context.Orders.FirstOrDefault(o => o.Id == orderId);
+    }
+    public void UpdateOrder(Order order)
+    {
+        _context.SaveChanges();
+    }
 }
