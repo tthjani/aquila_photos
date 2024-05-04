c17e9c2c40cdbb1dae2dae319186cb4a10cec3d9
tthjani <tothje98@gmail.com>
2024-05-04 12:23:57 +0200

    feat: added FinishedOrdersRepository

---
+++ APhoto-backend/Services/Repository/FinishedOrdersRepository.cs
@@ -0,0 +1,25 @@
+﻿using APhoto_backend.Data;
+using APhoto_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APhoto_backend.Services.Repository;
+
+public class FinishedOrdersRepository : IFinishedOrdersRepository
+{
+    private readonly APhotosContext _context;
+
+    public FinishedOrdersRepository(APhotosContext context)
+    {
+        _context = context;
+    }
+    
+    public Task<List<FinishedOrder>> GetAllFinishedOrders()
+    {
+        return _context.FinishedOrders.ToListAsync();
+    }
+    public void AddFinishedOrder(FinishedOrder finishedOrder)
+    {
+        _context.FinishedOrders.Add(finishedOrder);
+        _context.SaveChanges();
+    }
+}
