9cb6c5ad8d0decd2a170a482b78e783a1ba9d87b
tthjani <tothje98@gmail.com>
2024-05-04 12:27:10 +0200

    feat: added db configuration to the context class

--- APhoto-backend/Data/APhotosContext.cs
+++ APhoto-backend/Data/APhotosContext.cs
@@ -5,12 +5,10 @@ namespace APhoto_backend.Data;
 
 public class APhotosContext : DbContext
 {
-
     public APhotosContext(DbContextOptions<APhotosContext> options) : base(options)
     {
     }
     
-
     public DbSet<Order> Orders { get; set; }
     public DbSet<Gallery> Galleries { get; set; }
     public DbSet<Holiday> Holidays { get; set; }
@@ -18,4 +16,13 @@ public class APhotosContext : DbContext
     public DbSet<Announcement> Announcements { get; set; }
     public DbSet<FinishedOrder> FinishedOrders { get; set; }
     public DbSet<DeclinedOrder> DeclinedOrders { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AcceptedOrder>().ToTable("AcceptedOrders");
+        modelBuilder.Entity<DeclinedOrder>().ToTable("DeclinedOrders");
+        modelBuilder.Entity<FinishedOrder>().ToTable("FinishedOrders");
+        
+        base.OnModelCreating(modelBuilder);
+    }
 }
