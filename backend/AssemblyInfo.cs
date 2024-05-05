8fc1f7e21984b1ba8d060020dbb66abff3d4cfec
Gergely Illyes <gergo.illyes@gmail.com>
2024-05-05 20:40:21 +0200

    AV bump

--- backend/AssemblyInfo.cs
+++ backend/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 
 [assembly: UserSecretsId("d8258b82-aa4d-431b-864c-00d883d4ce06")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
