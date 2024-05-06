e3fdcfb96a2342caa7d0503fbf9b8f87f68b9063
Gergely Illyes <gergo.illyes@gmail.com>
2024-05-06 15:27:15 +0200

    Adding StatusController

---
+++ backend/src/APhoto.Api/Controllers/StatusController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace APhoto.Api.Controllers
+{
+    [AllowAnonymous]
+    public class StatusController : Controller
+    {
+        [HttpGet]
+        [Route("/version")]
+        public IActionResult Version()
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version!;
+            return Ok(version.ToString());
+        }
+
+        [HttpGet]
+        [Route("/ping")]
+        public IActionResult Ping()
+        {
+            return Ok("pong");
+        }
+    }
+}
