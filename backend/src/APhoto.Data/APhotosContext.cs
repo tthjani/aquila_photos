0a04a9ac023d5417a257dbbb84f4219a7a982300
Gergely Illyes <gergo.illyes@gmail.com>
2024-05-07 12:03:55 +0200

    Adding Holiday table to the database schema

--- backend/src/APhoto.Data/APhotosContext.cs
+++ backend/src/APhoto.Data/APhotosContext.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace APhoto.Data;
 
 public class APhotosContext(DbContextOptions<APhotosContext> options) : DbContext(options)
 {
     public DbSet<Order> Order { get; set; }
-    
+
+    public DbSet<Holiday> Holiday { get; set; }
+
     // Below Datasets are commented, because they're not required to be deployed in the database, their schema isn't properly planned yet
     //public DbSet<Gallery> Gallery { get; set; }
-    //public DbSet<Holiday> Holiday { get; set; }
     //public DbSet<Announcement> Announcement { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -37,6 +39,19 @@ public class APhotosContext(DbContextOptions<APhotosContext> options) : DbContex
         modelBuilder.Entity<Order>()
             .Property(p => p.OrderStatus)
             .IsRequired();
+
+        modelBuilder.Entity<Holiday>()
+            .HasKey(k => k.HolidayId);
+        modelBuilder.Entity<Holiday>()
+            .Property(p => p.StartDate)
+            .IsRequired();
+        modelBuilder.Entity<Holiday>()
+            .Property(p => p.EndDate)
+            .IsRequired();
+        modelBuilder.Entity<Holiday>()
+            .Property(p => p.AllowOrders)
+            .IsRequired()
+            .HasDefaultValue(false);
 
         base.OnModelCreating(modelBuilder);
     }
--- backend/src/APhoto.Data/Holiday.cs
+++ backend/src/APhoto.Data/Holiday.cs
@@ -1,8 +1,15 @@
-﻿namespace APhoto.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APhoto.Data;
 
 public class Holiday
 {
-    public uint Id { get; set; }
-    public DateTime StartHoliday { get; set; }
-    public DateTime EndHoliday { get; set; }
+    public uint HolidayId { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime StartDate { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime EndDate { get; set; }
+    [MaxLength(250)]
+    public string? Comment { get; set; }
+    public bool AllowOrders { get; set; }
 }
